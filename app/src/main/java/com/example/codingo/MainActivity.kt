1e090ae6b523699aed70ff594f353a30819ff65e
dtscott9 <dtscott9@byui.edu>
2022-10-24 13:12:16 -0600

    added intent

--- app/src/main/java/com/example/codingo/MainActivity.kt
+++ app/src/main/java/com/example/codingo/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.codingo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var signIn : Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signIn = findViewById(R.id.signIn);
+
+        signIn.setOnClickListener {
+            val intent = Intent(this, com.example.codingo.Map::class.java)
+            startActivity(intent)
+            finish();
+        }
+
+
     }
 }
