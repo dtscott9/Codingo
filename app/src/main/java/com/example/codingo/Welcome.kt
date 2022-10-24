943dfec96c0836d1f2ed0dcddda0611ed39b81c6
dtscott9 <dtscott9@byui.edu>
2022-10-24 15:36:36 -0600

    added intents

--- app/src/main/java/com/example/codingo/Welcome.kt
+++ app/src/main/java/com/example/codingo/Welcome.kt
@@ -1,11 +1,29 @@
 package com.example.codingo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Welcome : AppCompatActivity() {
+    private lateinit var tips: Button
+    private lateinit var start: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        tips = findViewById(R.id.tips)
+        start = findViewById(R.id.unitId)
+
+        tips.setOnClickListener{
+            val intent = Intent(this, activity_tips::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        start.setOnClickListener{
+            val intent1 = Intent(this, practice_mchoice::class.java)
+            startActivity(intent1)
+            finish()
+        }
     }
 }
